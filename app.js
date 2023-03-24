52b3c00c7f4bf4efcac3edcf71c7d1ccf3b6b44e
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-03-24 07:47:54 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Covid from "./component/covid";
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
+
+const App = () => {
+
+}
+  return (
+    <>
+   
+     <covid/> 
+    </>
+  )
+
+
+export default App
+
+
